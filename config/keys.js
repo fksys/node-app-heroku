7b1f301d3810740612839302a0e58ad7f607a685
Nicolas <n.carrenoh2019@gmail.com>
2020-01-20 14:01:26 -0300

    env process and oauth flow

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,8 @@
+/* eslint-disable global-require */
 if (process.env.NODE_ENV === 'production') {
-  //
+  // production
+  module.exports = require('./prod.js');
 } else {
-  //
+  // development environment
+  module.exports = require('./dev');
 }
