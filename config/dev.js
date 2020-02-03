4a7d793bb3be0c3beab252a72afd25130edb2a57
Nicolas <n.carrenoh2019@gmail.com>
2020-02-03 15:45:16 -0300

    (config) config files

--- config/dev.js
+++ config/dev.js
@@ -3,4 +3,6 @@ module.exports = {
   googleClientSecret: 'L3aD6tSM0cAldqxltj4_8Qzm',
   mongoURI: 'mongodb+srv://fksys-dev:agata2018.@node-app-oacnq.mongodb.net/test?retryWrites=true&w=majority',
   cookieKey: '12asd123asdad13123easdasd12341tafsdasdvdkale12351l52167l8656215l1lasdaksdk23213',
+  stripePublishableKey: 'pk_test_X3bcR8gB740BYmKbv4k1vO6C00c4vjtgZC',
+  stripeSecretKey: 'sk_test_Qr2IG2XR0xj51cyM4IhnrSzu003h3yRqUX',
 };
