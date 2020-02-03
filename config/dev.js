cc7c25d60884be24017776a1410ca37c8dc7b9c7
Nicolas Carreño <n.carrenoh2019@gmail.com>
2020-02-03 16:04:14 -0300

    d d

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID: '869507390179-ea9smcjin0j2vj2bftut69719vhckj7t.apps.googleusercontent.com',
-  googleClientSecret: 'L3aD6tSM0cAldqxltj4_8Qzm',
-  mongoURI: 'mongodb+srv://fksys-dev:agata2018.@node-app-oacnq.mongodb.net/test?retryWrites=true&w=majority',
-  cookieKey: '12asd123asdad13123easdasd12341tafsdasdvdkale12351l52167l8656215l1lasdaksdk23213',
-  stripePublishableKey: 'pk_test_X3bcR8gB740BYmKbv4k1vO6C00c4vjtgZC',
-  stripeSecretKey: 'sk_test_Qr2IG2XR0xj51cyM4IhnrSzu003h3yRqUX',
-};
